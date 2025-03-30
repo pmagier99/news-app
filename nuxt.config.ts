d544a634608373128fc3de845027e7d86070d695
oomph-01 <pmagier99@gmail.com>
2025-03-30 19:29:56 +0100

    change config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    head:{
+      meta: [
+        {
+          name: "viewport", content: "width=device-width, initial-scale=1"
+        }
+      ]
+    }
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint", "@storyblok/nuxt"],
